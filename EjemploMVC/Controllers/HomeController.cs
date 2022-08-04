3a4a06f11954771fb898f16e51b68c612d957290
Abel Palma <apalma@tsc.com.gt>
2022-08-03 20:55:40 -0600

    Add project files.

---
+++ EjemploMVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using EjemploMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EjemploMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any)]
+        public IActionResult Index()
+        {
+            _logger.LogError("Esto es un error");
+            _logger.LogWarning("Esto es un warning");
+            _logger.LogInformation("Esto solo es un mensaje");
+            return View();
+        }
+
+        [Route("privo")]
+        [Authorize(Roles="Administrators")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
